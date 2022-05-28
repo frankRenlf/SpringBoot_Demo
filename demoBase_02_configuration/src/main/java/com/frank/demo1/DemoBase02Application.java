322cecd9d8559ee7e4fd7a1c6d7b13c6abd742c6
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-05-28 14:14:33 +0800

    test base skills

---
+++ demoBase_02_configuration/src/main/java/com/frank/demo1/DemoBase02Application.java
@@ -0,0 +1,13 @@
+package com.frank.demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBase02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoBase02Application.class, args);
+    }
+
+}
---
+++ demoBase_02_configuration/src/main/java/com/frank/demo1/controller/BookController.java
@@ -0,0 +1,28 @@
+package com.frank.demo1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : demo1
+ * @Package : com.frank.demo1.controller
+ * @createTime : 2022/5/27 12:01
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf?tab=overview&from=2022-04-01&to=2022-04-30
+ * @Description :
+ */
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+    @GetMapping
+    public String getById() {
+
+        return "OPS! Success";
+    }
+}
