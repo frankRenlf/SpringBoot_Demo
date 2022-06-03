16f82c131f7c93fc76f6caf5bfc82037bb03c4d3
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-06-03 17:14:08 +0800

    test applicationTest based on class

--- demo_04_junit/src/test/java/com/frank/Demo04JunitApplicationTests.java
+++ demo_04_junit/src/test/java/com/frank/Demo04JunitApplicationTests.java
@@ -1,11 +1,12 @@
-package com.frank.demo_04_junit;
+package com.frank;
 
+import com.frank.demo_04_junit.Demo04JunitApplication;
 import com.frank.demo_04_junit.dao.BookDao;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = Demo04JunitApplication.class)
 class Demo04JunitApplicationTests {
 
     // Test object
