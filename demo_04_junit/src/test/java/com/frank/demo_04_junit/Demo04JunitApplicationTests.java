36abd2d6c16a64a27ce05937dd8511001aa533dc
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-06-03 16:03:39 +0800

    modify plugins

---
+++ demo_04_junit/src/test/java/com/frank/demo_04_junit/Demo04JunitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frank.demo_04_junit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo04JunitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
