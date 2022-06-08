07ac8005ee483dcaa8486caa49ec651f2590a0f2
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-06-08 12:57:26 +0800

    create new maven resp

---
+++ new_maven_text/src/main/java/com/frank/new_maven_text/NewMavenTextApplication.java
@@ -0,0 +1,13 @@
+package com.frank.new_maven_text;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewMavenTextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewMavenTextApplication.class, args);
+	}
+
+}
---
+++ new_maven_text/src/test/java/com/frank/new_maven_text/NewMavenTextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frank.new_maven_text;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewMavenTextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
