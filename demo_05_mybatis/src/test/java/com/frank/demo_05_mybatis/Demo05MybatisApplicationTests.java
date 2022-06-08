4662cc172e3db54897e9a07ae8a35b637643aac5
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-06-08 15:40:41 +0800

    test mybatis with mysql

--- demo_05_mybatis/src/test/java/com/frank/demo_05_mybatis/Demo05MybatisApplicationTests.java
+++ demo_05_mybatis/src/test/java/com/frank/demo_05_mybatis/Demo05MybatisApplicationTests.java
@@ -1,13 +1,19 @@
 package com.frank.demo_05_mybatis;
 
+import com.frank.demo_05_mybatis.dao.BookDao;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Demo05MybatisApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private BookDao bookDao;
+
+    @Test
+    void contextLoads() {
+        System.out.println(bookDao.getById(2));
+    }
 
 }
