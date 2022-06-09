697efbc6883315736fc4becea1edddbbc8369e8c
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-06-09 15:39:39 +0800

    separate two configurations

--- demo_07_druid/src/test/java/com/frank/demo_07_druid/Demo07DruidApplicationTests.java
+++ demo_07_druid/src/test/java/com/frank/demo_07_druid/Demo07DruidApplicationTests.java
@@ -1,13 +1,18 @@
 package com.frank.demo_07_druid;
 
+import com.frank.demo_07_druid.dao.BookDao;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Demo07DruidApplicationTests {
 
+    @Autowired
+    private BookDao bookDao;
     @Test
     void contextLoads() {
+        System.out.println(bookDao.getById(10));
     }
 
 }
