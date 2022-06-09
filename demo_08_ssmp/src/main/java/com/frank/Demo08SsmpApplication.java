d1d802b41663beca175e38b02a0ba96b4fc8fd80
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-06-09 15:45:07 +0800

    initiate ssm project

---
+++ demo_08_ssmp/src/main/java/com/frank/Demo08SsmpApplication.java
@@ -0,0 +1,13 @@
+package com.frank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo08SsmpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo08SsmpApplication.class, args);
+    }
+
+}
