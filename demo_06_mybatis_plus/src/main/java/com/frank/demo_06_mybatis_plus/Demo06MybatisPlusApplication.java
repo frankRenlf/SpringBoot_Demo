9656f5e3dac99214e4dffabb2c3c6c8bce8a97d7
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-06-09 14:32:56 +0800

    initiate mybatis_plus project

---
+++ demo_06_mybatis_plus/src/main/java/com/frank/demo_06_mybatis_plus/Demo06MybatisPlusApplication.java
@@ -0,0 +1,13 @@
+package com.frank.demo_06_mybatis_plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo06MybatisPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo06MybatisPlusApplication.class, args);
+    }
+
+}
---
+++ demo_06_mybatis_plus/src/test/java/com/frank/demo_06_mybatis_plus/Demo06MybatisPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frank.demo_06_mybatis_plus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo06MybatisPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
